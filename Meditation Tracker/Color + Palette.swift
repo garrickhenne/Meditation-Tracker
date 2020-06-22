1be3c2ff2bb653e6e02f27d9802a33c3095dd5ec
Garrick Henne <garrickhenne@outlook.com>
2020-06-21 19:15:02 -0700

    Changed colour palettes. Added stop button functionality

---
+++ Meditation Tracker/Color + Palette.swift
@@ -0,0 +1,16 @@
+//
+//  Color + newColors.swift
+//  Meditation Tracker
+//
+//  Created by Garrick Henne on 2020-06-21.
+//  Copyright © 2020 Garrick Henne. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let paletteBlack = Color("paletteBlack")
+    
+}
+
--- Meditation Tracker/MeditationTimerView.swift
+++ Meditation Tracker/MeditationTimerView.swift
@@ -20,57 +20,68 @@ struct MeditationTimerView: View {
     var body: some View {
         TabView {
             // MARK: Meditation Timer View
-            VStack {
-                Text(convertSecondsToMinutesAndSeconds(seconds: timerManager.timeRemaining))
-                    .font(.custom("Avenir", size: 50))
-                    .padding(.top, 200)
-                    .padding(.bottom, 10)
-                    
-                // TODO: - Add Animation on switches between pause and play.
-                if !self.isPlaying {
-                    Button(action: {
-                        self.timerManager.fireTimer()
-                        self.isPlaying = true
-                    },
-                           label: {
-                            Image(systemName: "play.circle.fill").resizable().frame(width: pausePlayButtonSize, height: pausePlayButtonSize, alignment: .center)
-                    })
-                        .padding(.bottom)
-                    //.shadow(radius: 5)
-                } else {
-                    Button(action: {
-                        self.timerManager.pause()
-                        self.isPlaying = false
-                    },
-                           label: {
-                            Image(systemName: "pause.circle.fill").resizable().frame(width: pausePlayButtonSize, height: pausePlayButtonSize, alignment: .center)
-                    })
-                        .padding(.bottom)
+            ZStack {
+                Rectangle()
+                    .fill(Color("paletteBlack"))
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text(convertSecondsToMinutesAndSeconds(seconds: timerManager.timeRemaining))
+                        .font(.custom("Avenir", size: 50))
+                        .foregroundColor(Color("paletteOrange"))
+                        .padding(.top, 200)
+                        .padding(.bottom, 10)
+                    HStack(spacing: 20) {
+                        // TODO: - Add Animation on switches between pause and play.
+                        if !self.isPlaying {
+                            Button(action: {
+                                self.timerManager.fireTimer()
+                                self.isPlaying = true
+                            },
+                                   label: {
+                                    Image(systemName: "play.circle.fill").resizable().frame(width: pausePlayButtonSize, height: pausePlayButtonSize, alignment: .center)
+                            })
+                        } else {
+                            Button(action: {
+                                self.timerManager.pause()
+                                self.isPlaying = false
+                            },
+                                   label: {
+                                    Image(systemName: "pause.circle.fill").resizable().frame(width: pausePlayButtonSize, height: pausePlayButtonSize, alignment: .center)
+                            })
+                        }
+                        Button(action: {
+                            self.timerManager.reset()
+                            self.isPlaying = false
+                        }, label: {
+                            Image(systemName: "stop.circle.fill").resizable().frame(width: pausePlayButtonSize, height: pausePlayButtonSize, alignment: .center)
+                        })
+                    }
+                    .padding()
+                    HStack {
+                        Button(action: {
+                            self.timerManager.setMeditationTime(from: 5*60)
+                            // Sound here was for testing
+                            playSound(sound: "LowBowlPadBeatMidVel4", type: "mp3")
+                        }) {
+                            TimerButton(label: "5 Minutes", buttonColor: Color("paletteOrange"))
+                        }
+                        
+                        Button(action: {self.timerManager.setMeditationTime(from: 10*60)}) {
+                            TimerButton(label: "10 Minutes", buttonColor: Color("paletteOrange"))
+                        }
+                    }
+                    .padding(.bottom)
+                    HStack {
+                        Button(action: {self.timerManager.setMeditationTime(from: 15*60)}) {
+                            TimerButton(label: "15 Minutes", buttonColor: Color("paletteOrange"))
+                        }
+                        Button(action: {self.timerManager.setMeditationTime(from: 20*60)}) {
+                            TimerButton(label: "20 Minutes", buttonColor: Color("paletteOrange"))
+                        }
+                    }
+                    .padding(.bottom)
+                    Spacer()
                 }
-                HStack {
-                    Button(action: {
-                        self.timerManager.setMeditationTime(from: 5*60)
-                        // Sound here was for testing
-                        playSound(sound: "LowBowlPadBeatMidVel4", type: "mp3")
-                    }) {
-                        TimerButton(label: "5 Minutes", buttonColor: .blue)
-                    }
-                    
-                    Button(action: {self.timerManager.setMeditationTime(from: 10*60)}) {
-                        TimerButton(label: "10 Minutes", buttonColor: .blue)
-                    }
-                }
-                .padding(.bottom)
-                HStack {
-                    Button(action: {self.timerManager.setMeditationTime(from: 15*60)}) {
-                        TimerButton(label: "15 Minutes", buttonColor: .blue)
-                    }
-                    Button(action: {self.timerManager.setMeditationTime(from: 20*60)}) {
-                        TimerButton(label: "20 Minutes", buttonColor: .blue)
-                    }
-                }
-                .padding(.bottom)
-                Spacer()
             }
              .tabItem {
                 Image(systemName: "moon")
@@ -92,6 +103,11 @@ struct MeditationTimerView: View {
                     Text("Settings")
             }
         }
+    .accentColor(Color("paletteOrange"))
+        .onAppear() {
+            UITabBar.appearance().isTranslucent = false
+            UITabBar.appearance().barTintColor = UIColor(red: 41 / 255, green: 50 / 255, blue: 65 / 255, alpha: 1.0)
+        }
     }
 }
 
@@ -109,7 +125,7 @@ struct TimerButton: View {
     var body: some View {
         Text(label)
             .padding(20)
-            .foregroundColor(.white)
+            .foregroundColor(Color("paletteBlack"))
             .background(buttonColor)
             .cornerRadius(10)
             //.shadow(radius: 5)
